465514aa14568959be5560b1f478cbef36992e83
rettofe <ferettosan@gmail.com>
2024-11-14 16:33:03 +0900

    エラー解消

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_28_074851) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_03_074609) do
+  create_table "installs", charset: "utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_installs_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true
+  end
+
   create_table "items", charset: "utf8", force: :cascade do |t|
     t.string "title", null: false
     t.text "description", null: false
